f76351fc9ee1afc1dcbee2a4141a402cfcfacb1a
Joshua Hill <posixninja@gmail.com>
2014-12-11 10:37:25 -0500

    Fixed the autoge.sh file so it'll work on mac and linux better. Fixed pkg-config file to add the right library to link

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
-aclocal
-libtoolize
+#!/bin/bash
+gprefix=`which glibtoolize 2>&1 >/dev/null`
+if [ $? -eq 0 ]; then 
+  glibtoolize --force
+else
+  libtoolize --force
+fi
+aclocal -I m4
 autoheader
-autoconf
 automake --add-missing
-autoreconf -i
-./configure
+autoconf
+
+if [ -z "$NOCONFIGURE" ]; then
+    ./configure "$@"
+fi
